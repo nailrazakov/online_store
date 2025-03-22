d21f4384e5d30f36a7b5480804d97568371f5f20
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2025-03-23 00:40:57 +0300

    django ORM

---
+++ catalog/management/commands/load_fixture.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Category, Product
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = 'delete all data from database and fill some data'
+
+    def handle(self, *args, **kwargs):
+        # Удаляем существующие записи
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+        call_command('loaddata', 'catalog_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Класс для модели категория"""
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Название категории",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание категории"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание",
+        help_text="Введите описание продукта",
+    )
+    image = models.ImageField(
+        upload_to="media/",
+        blank=True,
+        null=True,
+        verbose_name="изображение",
+        help_text="Загрузите изображение продукта",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Категория",
+        related_name="products",
+    )
+    price = models.DecimalField(
+        verbose_name="Цена", max_digits=10, decimal_places=2
+    )
+    #  дата создания
+    created_at = models.DateTimeField(auto_now_add=True)
+    #  дата последнего изменения
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
