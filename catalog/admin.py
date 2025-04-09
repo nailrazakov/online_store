a9646236d1c9b0ca69106c2e02d4152b1c5b8be4
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2025-04-09 12:49:47 +0300

    exercise first FBV to CBV

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,11 +4,22 @@ from catalog.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', )
+    list_display = (
+        "id",
+        "name",
+    )
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category', )
-    list_filter = ('category', )
-    search_fields = ('name', 'description', )
+    list_display = (
+        "id",
+        "name",
+        "price",
+        "category",
+    )
+    list_filter = ("category",)
+    search_fields = (
+        "name",
+        "description",
+    )
--- catalog/management/commands/load_fixture.py
+++ catalog/management/commands/load_fixture.py
@@ -4,11 +4,11 @@ from django.core.management import call_command
 
 
 class Command(BaseCommand):
-    help = 'delete all data from database and fill some data'
+    help = "delete all data from database and fill some data"
 
     def handle(self, *args, **kwargs):
         # Удаляем существующие записи
         Category.objects.all().delete()
         Product.objects.all().delete()
-        call_command('loaddata', 'catalog_fixture_1.json')
-        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
+        call_command("loaddata", "catalog_fixture_1.json")
+        self.stdout.write(self.style.SUCCESS("Successfully loaded data from fixture"))
