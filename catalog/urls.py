c430e51ae3ff8d6dd3e958aee9ba1b92421b1912
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2025-06-18 00:09:40 +0300

    task_2 cache for one page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
+from django.views.decorators.cache import cache_page
 from catalog.views import (
     ProductListView,
     ProductDetailView,
@@ -14,7 +15,7 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("product/list/", ProductListView.as_view(), name="product_list"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/<int:pk>/", cache_page(60 * 15)(ProductDetailView.as_view()), name="product_detail"),
     path("product/create/", ProductCreateView.as_view(), name="product_create"),
     path("product/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
     path("product/delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
